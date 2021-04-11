d949f869a83f3b64a53eddb73147d1229d40ef55
Carson Gray <graycar@oregonstate.edu>
2021-04-10 21:17:50 -0700

    getting it to drive

--- steering_calibration.py
+++ steering_calibration.py
@@ -9,13 +9,6 @@ __reset_mcu__()
 time.sleep(0.01)
 
 car.set_dir_servo_angle(0)
-time.sleep(1)
-car.set_dir_servo_angle(30)
-time.sleep(2)
-car.set_dir_servo_angle(0)
-time.sleep(2)
-car.set_dir_servo_angle(-30)
-time.sleep(2)
 
 #car.dir_servo_angle_calibration()
 
