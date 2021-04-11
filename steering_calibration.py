3c5a3e311eee773248ff13c42e48e4b03d041be7
Carson Gray <graycar@oregonstate.edu>
2021-04-10 21:27:57 -0700

    getting it to drive

--- steering_calibration.py
+++ steering_calibration.py
@@ -6,8 +6,15 @@ time.sleep(0.01)
 import picarx_improved as car
 
 time.sleep(0.01)
-
+car.forward(30)
 car.set_dir_servo_angle(0)
+time.sleep(1)
+car.set_dir_servo_angle(15)
+time.sleep(2)
+car.set_dir_servo_angle(0)
+time.sleep(1)
+car.set_dir_servo_angle(-15)
+time.sleep(2)
 
 #car.dir_servo_angle_calibration()
 
