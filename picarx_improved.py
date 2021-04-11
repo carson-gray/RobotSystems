dfd5187ef000a5f01f660ef091cf33b6d8c224c8
Carson Gray <graycar@oregonstate.edu>
2021-04-10 21:51:48 -0700

    getting it to drive

--- picarx_improved.py
+++ picarx_improved.py
@@ -197,11 +197,11 @@ def forward(speed):
         right_ratio = abs(right_radius / turning_radius)
         left_ratio = abs(left_radius / turning_radius)
         if right_ratio > left_ratio:
-            set_motor_speed(1, -1 * left_ratio * speed / 4)
+            set_motor_speed(1, -1 * left_ratio * speed / 10)
             set_motor_speed(2, -1 * right_ratio * speed)
         else:
             set_motor_speed(1, -1 * left_ratio * speed)
-            set_motor_speed(2, -1 * right_ratio * speed / 4)
+            set_motor_speed(2, -1 * right_ratio * speed / 10)
     else:
         set_motor_speed(1, -1 * speed)
         set_motor_speed(2, -1 * speed)
