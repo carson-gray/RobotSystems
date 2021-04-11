902deaf06fd1254c57c7c10828c48a35345ded04
Carson Gray <graycar@oregonstate.edu>
2021-04-10 20:39:10 -0700

    getting it to drive

--- steering_calibration.py
+++ steering_calibration.py
@@ -1,9 +1,9 @@
 import picarx_improved as car
 from ezblock import __reset_mcu__
-__reset_mcu__()
 import time
 
-car.set_dir_servo_angle(0)
+__reset_mcu__()
+
 car.forward(75)
 time.sleep(10)
 #car.dir_servo_angle_calibration()
